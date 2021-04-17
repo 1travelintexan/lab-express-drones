8fd23a7d73ae99d5019660d54d66016e951e79f0
Joshua George <horheyinc8@gmail.com>
2021-04-17 23:48:36 +0200

    connection made and Schema created

--- models/Drone.model.js
+++ models/Drone.model.js
@@ -1,1 +1,13 @@
 // Iteration #1
+const { Schema, model } = require("mongoose");
+
+//The schema or parameters for the drones to meet before they can enter the DB
+const droneSchema = new Schema({
+  name: String,
+  propellers: Number,
+  maxSpeed: Number,
+});
+
+const droneModel = model("Drone", droneSchema);
+
+module.exports = droneModel;
--- seeds/drones.seed.js
+++ seeds/drones.seed.js
@@ -1,1 +1,23 @@
 // Iteration #1
+//connect to DB with mongoose
+require("../db");
+
+// Seeded Drones (first in the DB)
+const myDrones = [
+  { name: "Sky Surfer", propellers: 4, maxSpeed: 18 },
+  { name: "The Twinny", propellers: 2, maxSpeed: 12 },
+  { name: "The John John", propellers: 6, maxSpeed: 25 },
+];
+
+const mongoose = require("mongoose");
+const Drones = require("../models/Drone.model.js");
+
+Drones.create({ myDrones })
+  .then(() => {
+    console.log("finally all good");
+    // close the connection
+    mongoose.connection.close();
+  })
+  .catch(() => {
+    console.log("There was a problem seeding!");
+  });
